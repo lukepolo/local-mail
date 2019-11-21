49ce8cf8a9dcc6929ec2dc255231e53c16832893
Luke Policinski <luke.policinski@egov.com>
2019-11-20 21:13:41 -0500

    removing some headers

--- views/layouts/components/Navigation.vue
+++ views/layouts/components/Navigation.vue
@@ -1,27 +1,10 @@
 <template>
   <header class="flex flex-shrink-0">
     <div class="flex-shrink-0 px-4 py-3 bg-gray-700 lg:w-64 lg:bg-gray-800">
-      <button class="block text-gray-400 hover:text-gray-200 sm:hidden">
-        <svg class="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
-          <path
-            d="M3 6a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM3 12a1 1 0 011-1h16a1 1 0 110 2H4a1 1 0 01-1-1zM4 17a1 1 0 100 2h7a1 1 0 100-2H4z"
-          />
-        </svg>
-      </button>
-
-      <button class="hidden sm:flex sm:items-center sm:w-full">
-        <img
-          class="h-8 w-8 rounded-full object-cover"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=144&h=144&q=80"
-          alt=""
-        />
-        <span class="hidden lg:inline ml-4 mr-2 text-sm font-medium text-white">
-          Monica White
-        </span>
-        <i
-          class="fa fa-chevron-down mt-2 ml-2 h-6 w-6 text-gray-400 lg:ml-auto"
-        ></i>
-      </button>
+      <span class="ml-4 mr-2 font-medium text-lg text-white italic">
+        <i class="fad fa-mailbox mr-2"></i>
+        Local Mail
+      </span>
     </div>
     <div
       class="flex-1 flex items-center justify-between pl-2 pr-6 bg-gray-700 lg:px-6"
