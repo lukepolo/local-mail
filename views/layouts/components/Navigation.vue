b3fffc0d71b34141562cbdbcd9c15d2e3db1bb33
Luke Policinski <luke.policinski@egov.com>
2019-11-20 21:26:34 -0500

    removing unused links for now

--- views/layouts/components/Navigation.vue
+++ views/layouts/components/Navigation.vue
@@ -18,26 +18,26 @@
           Developer Tools
         </router-link>
       </nav>
-      <div class="ml-auto flex items-center">
-        <button class="lg:hidden ml-5 text-gray-400 hover:text-gray-200">
-          <i class="fal fa-search fa-lg"></i>
-        </button>
-        <div class="hidden lg:block relative w-64">
-          <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-            <i class="fal fa-search fa-sm text-gray-500"></i>
-          </span>
-          <input
-            placeholder="Search"
-            class="pl-8 pr-4 py-2 w-full bg-gray-800 rounded-lg text-sm placeholder-gray-400 text-white focus:bg-white focus:placeholder-gray-600 focus:text-gray-900 focus:outline-none"
-          />
-        </div>
-        <button class="ml-5 text-gray-400 hover:text-gray-200">
-          <i class="fal fa-bell fa-lg"></i>
-        </button>
-        <a href="#" class="ml-4 text-gray-400 hover:text-gray-200">
-          <i class="fal fa-cog fa-lg"></i>
-        </a>
-      </div>
+<!--      <div class="ml-auto flex items-center">-->
+<!--        <button class="lg:hidden ml-5 text-gray-400 hover:text-gray-200">-->
+<!--          <i class="fal fa-search fa-lg"></i>-->
+<!--        </button>-->
+<!--        <div class="hidden lg:block relative w-64">-->
+<!--          <span class="absolute inset-y-0 left-0 flex items-center pl-3">-->
+<!--            <i class="fal fa-search fa-sm text-gray-500"></i>-->
+<!--          </span>-->
+<!--          <input-->
+<!--            placeholder="Search"-->
+<!--            class="pl-8 pr-4 py-2 w-full bg-gray-800 rounded-lg text-sm placeholder-gray-400 text-white focus:bg-white focus:placeholder-gray-600 focus:text-gray-900 focus:outline-none"-->
+<!--          />-->
+<!--        </div>-->
+<!--        <button class="ml-5 text-gray-400 hover:text-gray-200">-->
+<!--          <i class="fal fa-bell fa-lg"></i>-->
+<!--        </button>-->
+<!--        <a href="#" class="ml-4 text-gray-400 hover:text-gray-200">-->
+<!--          <i class="fal fa-cog fa-lg"></i>-->
+<!--        </a>-->
+<!--      </div>-->
     </div>
   </header>
 </template>
