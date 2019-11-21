1dccf0aedeeea2f9f99237a5a0ccd7d1db5c27d0
Luke Policinski <luke.policinski@egov.com>
2019-11-20 21:24:41 -0500

    removing mail actions for now

---
+++ app/components/mailbox/mail/components/MailActions.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex items-center justify-between px-5 py-4 bg-gray-100 border-b">
+    <div class="flex items-center">
+      <button>
+        <i class="fad fa-reply fa-lg text-gray-600"></i>
+      </button>
+      <button class="ml-6">
+        <button>
+          <i class="fad fa-tag fa-lg text-gray-600"></i>
+        </button>
+      </button>
+      <button class="ml-6">
+        <button>
+          <i class="fad fa-user-circle fa-lg text-gray-600"></i>
+        </button>
+      </button>
+      <button class="ml-6">
+        <button>
+          <i class="fad fa-download fa-lg text-gray-600"></i>
+        </button>
+      </button>
+      <button class="ml-6">
+        <button>
+          <i class="fad fa-ellipsis-h fa-lg text-gray-600"></i>
+        </button>
+      </button>
+    </div>
+    <div class="flex items-center">
+      <button>
+        <i class="fad fa-chevron-down fa-lg text-gray-600"></i>
+      </button>
+      <button class="ml-2">
+        <i class="fad fa-chevron-up fa-lg text-gray-600"></i>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+  export default {};
+</script>
--- app/components/mailbox/mail/components/MailHeader.vue
+++ app/components/mailbox/mail/components/MailHeader.vue
@@ -1,42 +1,6 @@
 <template>
   <div class="relative shadow-md">
-    <div
-      class="flex items-center justify-between px-5 py-4 bg-gray-100 border-b"
-    >
-      <div class="flex items-center">
-        <button>
-          <i class="fad fa-reply fa-lg text-gray-600"></i>
-        </button>
-        <button class="ml-6">
-          <button>
-            <i class="fad fa-tag fa-lg text-gray-600"></i>
-          </button>
-        </button>
-        <button class="ml-6">
-          <button>
-            <i class="fad fa-user-circle fa-lg text-gray-600"></i>
-          </button>
-        </button>
-        <button class="ml-6">
-          <button>
-            <i class="fad fa-download fa-lg text-gray-600"></i>
-          </button>
-        </button>
-        <button class="ml-6">
-          <button>
-            <i class="fad fa-ellipsis-h fa-lg text-gray-600"></i>
-          </button>
-        </button>
-      </div>
-      <div class="flex items-center">
-        <button>
-          <i class="fad fa-chevron-down fa-lg text-gray-600"></i>
-        </button>
-        <button class="ml-2">
-          <i class="fad fa-chevron-up fa-lg text-gray-600"></i>
-        </button>
-      </div>
-    </div>
+<!--    <mail-actions></mail-actions>-->
     <div class="flex items-center justify-between px-5 py-4 bg-white">
       <h3 class="text-base text-lg text-gray-900 truncate">
         {{ message.subject }}
@@ -51,11 +15,15 @@
 <script>
   import { format, parseISO } from "date-fns";
   import DateFormats from "@app/constants/DateFormats";
+  import MailActions from "@components/mailbox/mail/components/MailActions";
   export default {
     props: {
       message: {
         required: true,
       },
+    },
+    components: {
+      MailActions,
     },
     computed: {
       messageDate() {
